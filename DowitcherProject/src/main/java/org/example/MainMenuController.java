0690dc37a53282bf8b5827e5e8ec2315e38a57f7
Lucas Rettig <lucasrettig23@augustana.edu>
2024-10-03 11:40:29 -0500

    Added main menu buttons

--- DowitcherProject/src/main/java/org/example/MainMenuController.java
+++ DowitcherProject/src/main/java/org/example/MainMenuController.java
@@ -1,9 +1,19 @@
 package org.example;
 
 import javafx.fxml.FXML;
-
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
 import java.io.IOException;
 
 public class MainMenuController extends SwitchTo{
+    @FXML
+    private Button switchToLiveChatButton;
+
+    @FXML
+    private Button switchToPracticeMenuButton;
+
+    @FXML
+    private Button switchToSettingsButton;
+
 
 }
