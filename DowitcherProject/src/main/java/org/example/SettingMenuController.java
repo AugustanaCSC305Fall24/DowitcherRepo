bc8eefca785a2cbbc0b7ed629b0b6f6758fb4b64
Lucas Rettig <lucasrettig23@augustana.edu>
2024-10-03 12:08:59 -0500

    Creted Setting menu funtionality

--- DowitcherProject/src/main/java/org/example/SettingMenuController.java
+++ DowitcherProject/src/main/java/org/example/SettingMenuController.java
@@ -1,9 +1,25 @@
 package org.example;
 
 import javafx.fxml.FXML;
-
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
 import java.io.IOException;
 
 public class SettingMenuController extends SwitchTo {
+    @FXML
+    private Button switchToControlMenuButton;
 
+    @FXML
+    private Button switchToGameSettingButton;
+
+    @FXML
+    private Button switchToMainMenuButton;
+
+    @FXML
+    private Button switchToTutorialViewButton;
+
+    @FXML
+    private void handleTutorialButton(){
+        switchToTutorialViewButton.setText("No For This Sprint!");
+    }
 }
