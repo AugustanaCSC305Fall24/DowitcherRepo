e72d4e5591f991a9d4f8357854a5a18375ec5439
Brianfischer22 <brianfischer22@augustana.edu>
2024-10-10 11:41:00 -0500

    Practice typing changes

--- DowitcherProject/src/main/java/org/example/PracticeTypingController.java
+++ DowitcherProject/src/main/java/org/example/PracticeTypingController.java
@@ -1,4 +1,12 @@
 package org.example;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class PracticeTypingController extends SwitchTo{
+
+    @FXML
+    private Button practiceTypingBackButton;
+
+
 }
