7007975932e8ad238030c0c46ead8cf8d987e6c0
Lucas Rettig <lucasrettig23@augustana.edu>
2024-10-10 13:51:22 -0500

    Slight change to setting menu.

--- DowitcherProject/src/main/java/org/example/SettingMenuController.java
+++ DowitcherProject/src/main/java/org/example/SettingMenuController.java
@@ -5,7 +5,7 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import java.io.IOException;
 
-public class SettingMenuController extends SwitchTo {
+public class SettingMenuController extends SwitchTo{
     @FXML
     private Button switchToControlMenuButton;
 
