2ab0c7e0a2f3f4ccec5f2a1d4455eaa02d592a6a
tjklein23 <thomasklein23@augustana.edu>
2024-10-10 21:21:49 -0500

    Created checkTranslation method for practice listening mode (Currently sets text color red or green instead of highlight, . used to signify empty space

--- DowitcherProject/src/main/java/org/example/PracticeListeningController.java
+++ DowitcherProject/src/main/java/org/example/PracticeListeningController.java
@@ -2,8 +2,10 @@ package org.example;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
-
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 import java.io.IOException;
 
 public class PracticeListeningController{
@@ -15,8 +17,33 @@ public class PracticeListeningController{
     @FXML private Button replayAudioButton;
     @FXML private Button checkTranslationButton;
     @FXML private TextArea userInputTextArea;
-    @FXML private TextArea correctAnswerTextArea;
+    @FXML private ScrollPane correctAnswerScrollPane;
+    private String cwMessage = "Hello";
 
     @FXML private void switchToHomeScreenView() throws IOException{App.setRoot("HomeScreenView");}
+
+    @FXML
+    private void checkTranslation() {
+        String userTranslation = userInputTextArea.getText();
+        TextFlow correctedTranslation = new TextFlow();
+
+        for (int i = 0; i < userTranslation.length(); i++) {
+            Text checkedLetter;
+            if (i >= cwMessage.length()) {
+                checkedLetter = new Text(".");
+                checkedLetter.setStyle("-fx-fill: red;");
+            } else {
+                checkedLetter = new Text(Character.toString(cwMessage.charAt(i)));
+                if (userTranslation.charAt(i) == cwMessage.charAt(i)) {
+                    checkedLetter.setStyle("-fx-fill: green;");
+                } else {
+                    checkedLetter.setStyle("-fx-fill: red;");
+                }
+            }
+            correctedTranslation.getChildren().addAll(checkedLetter);
+        }
+
+        correctAnswerScrollPane.setContent(correctedTranslation);
+    }
 
 }
