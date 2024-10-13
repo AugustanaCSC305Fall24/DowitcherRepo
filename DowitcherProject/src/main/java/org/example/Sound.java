be01ba19e6cc0d95e33dca101f4399983fe9d9c8
ipalmroos <ian.palmroos@gmail.com>
2024-10-13 18:08:51 -0400

    created a method which plays a dit or dah sound depending on the input it receives

--- DowitcherProject/src/main/java/org/example/Sound.java
+++ DowitcherProject/src/main/java/org/example/Sound.java
@@ -1,5 +1,45 @@
 package org.example;
 
+import javax.sound.sampled.*;
+import java.io.IOException;
+import java.net.URL;
+
 public class Sound {
+    public static Clip audioClip;
+
+    // for the string use "/dit.wav" for dit and "/dah.wav" for dah
+    public static void playDitOrDah(String file) {
+        Mixer.Info[] mixerInfo = AudioSystem.getMixerInfo();  //find a mixer that can play the audio clip
+        Clip audioClip = null;
+        for (Mixer.Info info : mixerInfo) {
+            Mixer mixer = AudioSystem.getMixer(info);
+            DataLine.Info dataInfo = new DataLine.Info(Clip.class, null);
+
+            if (mixer.isLineSupported(dataInfo)) {
+                try {
+                    audioClip = (Clip) mixer.getLine(dataInfo);
+                } catch (LineUnavailableException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        try {
+            URL soundURL = Sound.class.getResource(file);
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(soundURL);
+            audioClip.open(audioStream);
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            e.printStackTrace();
+            return;
+        }
+        audioClip.start();
+        do {
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        } while (audioClip.isActive());
+        audioClip.close();
+    }
 
 }
