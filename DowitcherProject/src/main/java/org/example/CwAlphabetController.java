1ef2e386bd2f219b7619d98fc87f98151a037a72
tjklein23 <thomasklein23@augustana.edu>
2024-10-23 08:58:48 -0500

    Created and completed CwAlphabetView and created the controller for it

---
+++ DowitcherProject/src/main/java/org/example/CwAlphabetController.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.text.TextFlow;
+
+public class CwAlphabetController {
+
+    // All FXML elements on screen that are interacted with
+    @FXML private Button checkAnswerButton;
+    @FXML private Button skipNextButton;
+    @FXML private Button restartButton;
+    @FXML private Button settingsButton;
+    @FXML private Button mainMenuButton;
+    @FXML private ScrollPane previousTranslationsScrollPane;
+    @FXML private TextFlow currentLetterTextFlow;
+    @FXML private TextField userInputTextField;
+
+
+
+}
