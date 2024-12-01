68ea3835675edd48f4c0c7907155eaa992a2e976
lucas <lucasrettig23@augustana.edu>
2024-12-01 10:52:03 -0600

    Made it so the ai test and practice talking talk to the bot, ai test is in english and talking is in morse code.

--- DowitcherProject/src/main/java/module-info.java
+++ DowitcherProject/src/main/java/module-info.java
@@ -1,11 +1,12 @@
 module org.example {
     requires javafx.controls;
     requires javafx.fxml;
-    requires java.desktop;
     requires jdk.compiler;
     requires com.google.gson;
     requires com.google.auth;
     requires com.google.auth.oauth2;
+    requires com.fasterxml.jackson.databind;
+    requires java.desktop;
 //    requires org.jsoup;
 
 //    requires google.cloud.aiplatform;
